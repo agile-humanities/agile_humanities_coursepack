58facebdee77536582a08beb5d985d419c59cfa3
ajstanley <alanjarlathstanley@gmail.com>
2016-01-08 15:00:32 -0400

    moved menus

--- theme/agile_coursepack.tpl.php
+++ theme/agile_coursepack.tpl.php
@@ -7,6 +7,8 @@
 
 <div>
     <?php print $search_block; ?>
+    <?php print $solr_link; ?>
+    <br />
     <?php print $metadata_link; ?>
     <br />
     <?php print $consolidate_link; ?>
@@ -17,6 +19,6 @@
     <?php print $document; ?>
     <?php print $attribution ? t("Transmogrified from !link", array('!link' => $attribution)) : ''; ?>
     <p><?php print $contact; ?></p>
-    
+
     <?php print $links; ?>
 </div>
